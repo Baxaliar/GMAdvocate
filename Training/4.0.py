17e7f3b8b0bd1a76e634b99717fb02f3d04bb092
Baxaliar <justict21@gmail.com>
2021-03-19 08:27:06 +0200

    Solved the common mistake using mutable argument in default attribute

--- Training/4.0.py
+++ Training/4.0.py
@@ -1,16 +1,22 @@
 
-def vector_module(x, y, z):
-    return (x ** 2 + y ** 2 + z ** 2) ** .5
+def add_element_to_list(element, list_to_add=None):
+    """добавляем элемент к списку"""
+    if list_to_add is None:
+        list_to_add = []
+    list_to_add.append(element)
+    return list_to_add
 
 
-res = vector_module(2, y=3, z=4)
-print(res)
+# my_list = [3, 4, 5]
+# add_element_to_list(element=1, list_to_add=my_list)
+# print(my_list)
 
-# some_list = [2, 3, 4]
-# res = vector_module(*some_list)
-# print(res)
+new_list = add_element_to_list(element=1)
+print(new_list)
+add_element_to_list(element=7, list_to_add=new_list)
+print(new_list)
 
-some_list = [2, 3]
-some_dict = dict(z=4)
-res = vector_module(*some_list, **some_dict)
-print(res)
+other_new_list = add_element_to_list(element=2)
+print(other_new_list)
+print(new_list)
+print(other_new_list is new_list)
