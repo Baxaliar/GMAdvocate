4ba5b506686ee55fe84425d8d215501e03ce4fa7
Baxaliar <justict21@gmail.com>
2021-03-19 09:11:03 +0200

    Random number of keywords arguments

--- Training/4.0.py
+++ Training/4.0.py
@@ -9,3 +9,24 @@ def print_them_all_v1(*args):
 
 
 print_them_all_v1(2, 'привет', 5.6)
+
+# распаковка
+my_favorite_pets = ['lion', 'elephant', 'monkey', 'cat', 'horse']
+# print_them_all_v1(my_favorite_pets)
+print_them_all_v1(*my_favorite_pets)
+
+
+# Произвольное число позиционных параметров
+def print_them_all_v2(**kwargs):
+    print('print_them_all_v2')
+    print('тип kwargs:', type(kwargs))
+    print(kwargs)
+    for key, value in kwargs.items():
+        print('именнованный параметр:', key, '=', value)
+
+
+print_them_all_v2(name='Вася', address='Moscow')
+
+# Распаковка
+my_friend = {'name': 'Вася', 'address': 'Moscow', 'age': 25}
+print_them_all_v2(**my_friend)
