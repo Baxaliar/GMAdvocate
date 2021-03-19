77c24062c73f56cf6a4b2fd6257fc8ad31c0c055
Baxaliar <justict21@gmail.com>
2021-03-19 08:56:36 +0200

    Random number of position arguments

--- Training/4.0.py
+++ Training/4.0.py
@@ -1,22 +1,11 @@
 
-def add_element_to_list(element, list_to_add=None):
-    """добавляем элемент к списку"""
-    if list_to_add is None:
-        list_to_add = []
-    list_to_add.append(element)
-    return list_to_add
+# Произвольное число позиционных параметров
+def print_them_all_v1(*args):
+    print('print_them_all_v1')
+    print('тип args:', type(args))
+    print(args)
+    for i, arg in enumerate(args):
+        print('позиционный параметр:', i, arg)
 
 
-# my_list = [3, 4, 5]
-# add_element_to_list(element=1, list_to_add=my_list)
-# print(my_list)
-
-new_list = add_element_to_list(element=1)
-print(new_list)
-add_element_to_list(element=7, list_to_add=new_list)
-print(new_list)
-
-other_new_list = add_element_to_list(element=2)
-print(other_new_list)
-print(new_list)
-print(other_new_list is new_list)
+print_them_all_v1(2, 'привет', 5.6)
