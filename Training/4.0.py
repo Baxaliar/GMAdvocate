61d5dc80cf2a3fc9898c4946c6bc6c04debe5240
Baxaliar <justict21@gmail.com>
2021-03-19 09:41:45 +0200

    Combination of args and kwargs

--- Training/4.0.py
+++ Training/4.0.py
@@ -1,32 +1,22 @@
 
-# Произвольное число позиционных параметров
-def print_them_all_v1(*args):
-    print('print_them_all_v1')
+# Комбинация
+def print_them_all_v3(*args, **kwargs):
+    print('print_them_all_v3')
     print('тип args:', type(args))
     print(args)
     for i, arg in enumerate(args):
         print('позиционный параметр:', i, arg)
-
-
-print_them_all_v1(2, 'привет', 5.6)
-
-# распаковка
-my_favorite_pets = ['lion', 'elephant', 'monkey', 'cat', 'horse']
-# print_them_all_v1(my_favorite_pets)
-print_them_all_v1(*my_favorite_pets)
-
-
-# Произвольное число позиционных параметров
-def print_them_all_v2(**kwargs):
-    print('print_them_all_v2')
     print('тип kwargs:', type(kwargs))
     print(kwargs)
     for key, value in kwargs.items():
-        print('именнованный параметр:', key, '=', value)
+        print('именованный аргумент:', key, '=', value)
 
 
-print_them_all_v2(name='Вася', address='Moscow')
+print_them_all_v3('Вася', 'Moscow', 25)
+print_them_all_v3(name='Вася', address='Moscow')
+
+print_them_all_v3(1000, 'рублей', name='Вася', address='Moscow')
 
 # Распаковка
-my_friend = {'name': 'Вася', 'address': 'Moscow', 'age': 25}
-print_them_all_v2(**my_friend)
+my_friend = {'name': 'Вася', 'address': 'Moscow'}
+print_them_all_v3(1000, 'рублей', **my_friend)
