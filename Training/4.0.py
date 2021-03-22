56d9d62bb9244195669e4884c68fc998884e7bfa
Baxaliar <justict21@gmail.com>
2021-03-22 08:42:59 +0200

    Max factorial

--- Training/4.0.py
+++ Training/4.0.py
@@ -1,22 +1,10 @@
 
-# Комбинация
-def print_them_all_v3(*args, **kwargs):
-    print('print_them_all_v3')
-    print('тип args:', type(args))
-    print(args)
-    for i, arg in enumerate(args):
-        print('позиционный параметр:', i, arg)
-    print('тип kwargs:', type(kwargs))
-    print(kwargs)
-    for key, value in kwargs.items():
-        print('именованный аргумент:', key, '=', value)
+# Факториал
+def factorial(n):
+    if n == 1:
+        return 1
+    factorial_n_minus_1 = factorial(n=n - 1)
+    return n * factorial_n_minus_1
 
 
-print_them_all_v3('Вася', 'Moscow', 25)
-print_them_all_v3(name='Вася', address='Moscow')
-
-print_them_all_v3(1000, 'рублей', name='Вася', address='Moscow')
-
-# Распаковка
-my_friend = {'name': 'Вася', 'address': 'Moscow'}
-print_them_all_v3(1000, 'рублей', **my_friend)
+print(factorial(998))
