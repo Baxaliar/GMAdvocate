ddbf5560a30d9fe336209b554ef5e171d0bba75a
Baxaliar <justict21@gmail.com>
2021-04-07 11:33:49 +0300

    Fractal recursive function

--- Training/Practice.py
+++ Training/Practice.py
@@ -2,29 +2,25 @@
 import simple_draw as sd
 
 sd.resolution = (1366, 768)
+point_0 = sd.get_point(700, 5)
 
-
-def branch(point, angle, length):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw()
-    return v1.end_point
-
-
-# Цикл
-angle_0 = 90
-length_0 = 200
-point_0 = sd.get_point(100, 5)
-
-next_angle = angle_0
-next_length = length_0
-next_point = point_0
-
-while next_length > 1:
-    next_point = branch(point=next_point, angle=next_angle, length=next_length)
-    next_angle = next_angle - 30
-    next_length = next_length * .75
 
 # сделать функцию branch рекурсивной
+def branch(point, angle, length, delta):
+    if length < 1:
+        return
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+    next_point = v1.end_point
+    next_angle = angle - delta
+    next_length = length * .75
+    branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
+
+
+for delta in range(0, 51, 10):
+    branch(point=point_0, angle=90, length=150, delta=delta)
+for delta in range(-50, 1, 10):
+    branch(point=point_0, angle=90, length=150, delta=delta)
 
 
 sd.pause()
