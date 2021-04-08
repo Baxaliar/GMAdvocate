32efb6f04f3fa742722e4419ce747787b6d80495
Baxaliar <justict21@gmail.com>
2021-04-08 06:38:33 +0300

    Fractal recursive function snowflake

--- Training/Practice.py
+++ Training/Practice.py
@@ -2,25 +2,32 @@
 import simple_draw as sd
 
 sd.resolution = (1366, 768)
-point_0 = sd.get_point(700, 5)
 
+# sd.snowflake(center=point_0, length=200, factor_a=.5)
 
-# сделать функцию branch рекурсивной
-def branch(point, angle, length, delta):
-    if length < 1:
-        return
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw()
-    next_point = v1.end_point
-    next_angle = angle - delta
-    next_length = length * .75
-    branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
+y = 500
+x = 100
 
+y_2 = 450
+x_2 = 150
+while True:
+    sd.clear_screen()
+    point = sd.get_point(x, y)
+    sd.snowflake(center=point, length=50)
+    y -= 10
+    if y < 50:
+        break
+    x = x * 1.1
 
-for delta in range(0, 51, 10):
-    branch(point=point_0, angle=90, length=150, delta=delta)
-for delta in range(-50, 1, 10):
-    branch(point=point_0, angle=90, length=150, delta=delta)
+    point_2 = sd.get_point(x_2, y_2)
+    sd.snowflake(center=point_2, length=50)
+    y_2 -= 10
+    if y_2 < 50:
+        break
+    x_2 = x_2 * 1.1
 
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
 
 sd.pause()
