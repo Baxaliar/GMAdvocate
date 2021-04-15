d77dea022b703038b553c852767e828cf5c953f5
Baxaliar <justict21@gmail.com>
2021-04-15 11:05:25 +0300

    Namespaces and scopes learned

--- Training/Module 6/Practice.py
+++ Training/Module 6/Practice.py
@@ -1,11 +1,16 @@
 
-def func_3():
-    global x
-    x = 'LOCO'
-    print('x in f1 =', x)
+def f1():
+    def f2():
+        nonlocal x
+        x = 27
+        print('in f2 x =', x)
+
+    x = 34
+    print('in f1 x =', x)
+    f2()
+    print('in f1 x =', x)
 
 
-print(globals())
-func_3()
-print(x)
-print(globals())
+x = 42
+f1()
+print('x =', x)
