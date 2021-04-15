60662741928daf7fa2078f6b9f9f59ea25e7e03a
Baxaliar <justict21@gmail.com>
2021-04-15 05:58:49 +0300

    Created module my_burger.py - recipe of burger, completed homework 03

---
+++ Training/DZ5/my_burger.py
@@ -0,0 +1,46 @@
+
+def bread():
+    my_burger.append('Булочка')
+    print('А теперь добавим булочку')
+
+
+def meat():
+    my_burger.append('Котлета')
+    print('А теперь добавим котлету')
+
+
+def pickle():
+    my_burger.append('Огурчик')
+    print('А теперь добавим огурчик')
+
+
+def tomato():
+    my_burger.append('Помидорчик')
+    print('А теперь добавим помидорчик')
+
+
+def mayo():
+    my_burger.append('Майонез')
+    print('А теперь добавим майонез')
+
+
+def cheese():
+    my_burger.append('Сыр')
+    print('А теперь добавим сыр')
+
+
+def spice():
+    my_burger.append('Специи')
+    print('А теперь добавим специи')
+
+
+def onion():
+    my_burger.append('Лук')
+    print('А теперь добавим лучок')
+
+
+def recipe():
+    print('Рецпт бургера:', ', '.join(my_burger))
+
+
+my_burger = []
