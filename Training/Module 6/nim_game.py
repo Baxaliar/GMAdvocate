9d8d165ddd7e0f68e306b7cf042a59557f91ed4e
Baxaliar <justict21@gmail.com>
2021-04-16 10:23:18 +0300

    Prototype API added players_number

--- Training/Module 6/nim_game.py
+++ Training/Module 6/nim_game.py
@@ -2,11 +2,14 @@ from nim_engine import put_stones, get_bunches, take_from_bunch, gameover
 
 
 put_stones()
+user_number = 1
 while True:
     print('Текущая позиция')
     print(get_bunches())
+    print('Ход игрока {}'.format(user_number))
     pos = input('Откуда берём?')
     qua = input('Сколько берём?')
     take_from_bunch(position=int(pos), quantity=int(qua))
     if gameover():
         break
+    user_number = 2 if user_number == 1 else 1
