1e5887c6f3a3b295843b09942a7d3734389a06d0
Ivan Baxaliar <78634016+Baxaliar@users.noreply.github.com>
2021-12-08 07:15:43 +0200

    Delete Training/DZ5 directory

--- Training/DZ5/01_inhabitants.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Вывести на консоль жителей комнат (модули room_1 и room_2)
-# Формат: В комнате room_1 живут: ...
-from room_1 import folks as folks_1
-from room_2 import folks as folks_2
-
-print('В комнате room_1 живут:', folks_1, 'В комнате room_1 живут:', folks_2)
